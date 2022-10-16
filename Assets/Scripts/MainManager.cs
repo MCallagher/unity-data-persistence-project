277bb268610df45ad8581878432f850b7884a483
Marco <marco.calandro@mensa.it>
2022-10-16 18:01:12 +0200

    Add high score persistence
    - Added high score update
    - Added high score session persistence

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text HighScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -19,10 +21,14 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    
+    private HighScoreData highScoreData;
+    private string dataFilename;
+
     // Start is called before the first frame update
     void Start()
     {
+        dataFilename = $"{Application.persistentDataPath}/high_score_data.json";
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -40,6 +46,8 @@ public class MainManager : MonoBehaviour
             }
         }
         AddPoint(0);
+        LoadHighScore();
+        UpdateHighScore();
     }
 
     private void Update()
@@ -79,9 +87,49 @@ public class MainManager : MonoBehaviour
         }
     }
 
+    void UpdateHighScore()
+    {
+        HighScoreText.text = $"High score : {highScoreData.name} {highScoreData.score}";
+    }
+
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+        if (m_Points > highScoreData.score)
+        {
+            highScoreData.name = m_Player;
+            highScoreData.score = m_Points;
+            UpdateHighScore();
+            StoreHighScore();
+        }
+    }
+
+    [System.Serializable]
+    class HighScoreData
+    {
+        public string name;
+        public int score;
+    }
+
+    private void LoadHighScore()
+    {
+        if (File.Exists(dataFilename))
+        {
+            string json = File.ReadAllText(dataFilename);
+            highScoreData = JsonUtility.FromJson<HighScoreData>(json);
+        }
+        else
+        {
+            highScoreData = new HighScoreData();
+            highScoreData.name = "Nobody";
+            highScoreData.score = 0;
+        }
+    }
+
+    private void StoreHighScore()
+    {
+        string json = JsonUtility.ToJson(highScoreData);
+        File.WriteAllText(dataFilename, json);
     }
 }
